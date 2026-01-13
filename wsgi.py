d7f756116ed8e5582868b73859b80d595d0c5bdc
iamminhquan <minhquan542005@gmail.com>
2026-01-13 22:38:54 +0700

    chore: Rename from run.py into wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+from app import create_app
+
+app = create_app()
+
+
+def main():
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
+        load_dotenv=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
