b2ec949430e8cee900b7334ca935f30f26cbab4e
iamminhquan <minhquan542005@gmail.com>
2026-01-20 14:15:06 +0700

    refactor(auth): Refactor authentication UI and routing for login, registration, and logout

--- app/routes/main_route.py
+++ app/routes/main_route.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from flask_login import current_user, login_required
+from flask import Blueprint, redirect, render_template, session, url_for
+from flask_login import current_user, login_required, logout_user
 
 
 main_bp = Blueprint(
@@ -19,7 +19,25 @@ def show_about_page():
     return render_template("about.html")
 
 
+@main_bp.route("/login")
+def show_login_page():
+    return redirect(url_for("auth.show_sign_in_page"))
+
+
+@main_bp.route("/register")
+def show_register_page():
+    return redirect(url_for("auth.show_sign_up_page"))
+
+
 @main_bp.route("/account")
 @login_required
 def show_account_page():
     return render_template("account.html", user=current_user)
+
+
+@main_bp.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    session.clear()
+    return redirect(url_for("main.show_home_page"))
