52bfa0d72a557f33ed8a4020b33a40b778d859e0
iamminhquan <minhquan542005@gmail.com>
2026-01-20 12:27:09 +0700

    feat(auth): Enhance account access with user authentication and account page routing

--- app/routes/main_route.py
+++ app/routes/main_route.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import current_user, login_required
 
 
 main_bp = Blueprint(
@@ -16,3 +17,9 @@ def show_home_page():
 @main_bp.route("/about")
 def show_about_page():
     return render_template("about.html")
+
+
+@main_bp.route("/account")
+@login_required
+def show_account_page():
+    return render_template("account.html", user=current_user)
