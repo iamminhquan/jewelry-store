112435e1cea4c8fee78d9a5b885d5050259b0f19
iamminhquan <minhquan542005@gmail.com>
2026-01-21 21:08:44 +0700

    Modified route /account int main_route for user

--- app/routes/main_route.py
+++ app/routes/main_route.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, redirect, render_template, session, url_for
+from flask import Blueprint, redirect, render_template, session, url_for, request
 from flask_login import current_user, login_required, logout_user
+from app.extensions import db
 
 
 main_bp = Blueprint(
@@ -29,10 +30,24 @@ def show_register_page():
     return redirect(url_for("auth.show_sign_up_page"))
 
 
-@main_bp.route("/account")
+@main_bp.route("/account", methods=["GET", "POST"])
 @login_required
 def show_account_page():
-    return render_template("account.html", user=current_user)
+    if request.method == "POST":
+        user = current_user
+        user.ho_ten = request.form.get("ho_ten")
+        user.email = request.form.get("email")
+        user.so_dien_thoai = request.form.get("so_dien_thoai")
+        user.dia_chi = request.form.get("dia_chi")
+        user.gioi_tinh = request.form.get("gioi_tinh")
+        user.ngay_sinh = request.form.get("ngay_sinh") or None
+
+        db.session.commit()
+
+    return render_template(
+        "account.html",
+        user=current_user,
+    )
 
 
 @main_bp.route("/logout", methods=["GET"])
