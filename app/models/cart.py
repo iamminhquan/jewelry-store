57769d37990a4feae14bec6fe703b39523113ed0
iamminhquan <minhquan542005@gmail.com>
2026-01-22 22:18:45 +0700

    chore: Added constructor (__init__) for cart and cart_detail models

--- app/models/cart.py
+++ app/models/cart.py
@@ -4,6 +4,16 @@ from app.extensions import db
 class Cart(db.Model):
     __tablename__ = "GioHang"
 
+    def __init__(
+        self,
+        account_id,
+        created_at,
+        status,
+    ) -> None:
+        self.__account_id = account_id
+        self.__created_at = created_at
+        self.__status = status
+
     ma_gio_hang = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     ma_tai_khoan = db.Column(db.Integer, nullable=False)
--- app/models/cart_detail.py
+++ app/models/cart_detail.py
@@ -4,9 +4,21 @@ from app.extensions import db
 class CartDetail(db.Model):
     __tablename__ = "ChiTietGioHang"
 
-    ma_chi_tiet_gio_hang = db.Column(
-        db.Integer, primary_key=True, autoincrement=True
-    )
+    def __init__(
+        self,
+        cart_detail_id,
+        product_id,
+        quantity,
+        price_at,
+        created_at,
+    ) -> None:
+        self.__cart_detail_id = cart_detail_id
+        self.__product_id = product_id
+        self.__quantity = quantity
+        self.__price_at = price_at
+        self.__created_at = created_at
+
+    ma_chi_tiet_gio_hang = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     ma_gio_hang = db.Column(db.Integer, nullable=False)
 
